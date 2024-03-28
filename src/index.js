a044977f1943487494c4081bda405a9f59868a87
Divyank Sisodia <Divyank1540.be20@chitkara.edu.in>
2024-03-28 14:51:22 +0530

    project skeleton setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-q
+const express = require('express');
+
+const { PORT } = require('./config/ServerConfig')
+
+const app = express();
+
+const prepareAndStartServer = () => {
+    app.listen(PORT, ()=>{
+        console.log(`Server started at ${PORT}`)
+    })
+}
